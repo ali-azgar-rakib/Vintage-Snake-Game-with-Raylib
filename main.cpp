3cc1c73d49cd54453e4e78ed8142791b3905b112
Rakib <aliazgarrakib@gmail.com>
2024-09-11 00:27:29 +0600

    fixed path error

--- main.cpp
+++ main.cpp
@@ -11,17 +11,14 @@ int main() {
 
   InitWindow(cellSize * cellCount, cellSize * cellCount, "Snanke Game");
   SetTargetFPS(60);
-  Image image = LoadImage(
-      "/mnt/3C14D7F414D7AF60/code/c++/raylib_projects/snake/graphics/food.png");
+  Image image = LoadImage("../graphics/food.png");
 
   Food food = Food(cellSize, cellCount, image);
-  if (image.width) {
-    while (!WindowShouldClose()) {
-      BeginDrawing();
-      ClearBackground(green);
-      food.Draw(darkdGreen);
-      EndDrawing();
-    }
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(green);
+    food.Draw(darkdGreen);
+    EndDrawing();
   }
   CloseWindow();
   return 1;
